338ba1d9ab482b5ca2c4001c67f1683e564030a6
Łukasz W <inicjallw@gmail.com>
2021-11-30 18:37:15 +0100

    POSTE method tested

--- index.php
+++ index.php
@@ -2,14 +2,27 @@
 <html lang="pl">
 <head>
     <meta charset="utf-8"/>
+    <title> Formularz </title>
 </head>
 <body>
+
+    <h1>Zamowienie</h1>
+    <form action="zamowienie.php" method="post">
+        Ile pierwszych (0.99 PLN/szt):
+        <input type="text" name="pierwsze" />
+            <br><br>
+        Ile drugich (1.29 PLN/szt):
+        <input type="text" name="drugie"/>
+            <br><br> 
+        <input type="submit" value="Wyślij zamówienie"/>
+    </form>
 
 <?php
 //kod niejawny
 
-    echo "Witaj Świecie";
-
+ //   $imie="Łukasz";
+ //   echo 'Witaj '.$imie;
+ 
 ?>
 
 </body>
---
+++ zamowienie.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8"/>
+    <title> Zamowienie </title>
+</head>
+<body>
+
+
+<?php
+  $pierwsze_pole=$_POST["pierwsze"];
+  $drugie_pole=$_POST["drugie"];
+
+  echo "<h2>$pierwsze_pole"." "."$drugie_pole</h2>";
+?>
+
+</body>
+</html>
